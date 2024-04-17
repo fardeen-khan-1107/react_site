f21cc27ce4ab611a28a25e7da2a5409186c7892a
fardeen khan <fardeenmrkhan@gmail.com>
2024-04-17 19:29:12 +0530

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {},
     fontFamily: {
       khan: ["Jersey 20", 'sans-serif'],
