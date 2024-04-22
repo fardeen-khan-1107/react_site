416b1026ecdc041b12af4c6f714564ec25e914f7
fardeen khan <fardeenmrkhan@gmail.com>
2024-04-22 07:57:54 +0530

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,21 +38,19 @@
     return (
       <section className="sub1 w-full cover h-[55rem] ">
       <video src={video1} autoPlay loop className="absolute w-full h-[55rem] object-cover" />
-      <div className='flex '>
-      <ul className={`relative  top-20 text-sm text-white cursor-pointer transition-all duration-500  ease-linear ${open ?'top-12':'top-[-490px]'}`}>
-    <div className="w-full sm:block flex justify-around">
+      <div className='flex'>
+      <ul className={` relative top-12 border-2 justify-center w-full  text-sm text-white cursor-pointer transition-all duration-500  ease-linear ${open ?'sm:top-12':'sm:top-[-490px]'}`}>
+    <div className="w-full sm:block flex">
     {links.map(ups =>
-        <li className='py-1 sm:text-lg'> 
-          <a href="/" className='relative'>
-            {ups.name}
-          </a>
+        <li className='w-full  z-10 py-1 sm:text-lg'> 
+          <a href="/" className='w-full text-white justify-between '>{ups.name}</a>
         </li>
       )}
     </div>
       </ul>
     <div onClick={()=>setopen(!open)} className='absolute text-white right-5 top-0 hidden sm:block'>
       {
-        open ? <IoMenu className='w-7 h-7'/>:<HiMiniXMark className='w-7 h-7'/>
+        open ? <HiMiniXMark className='w-7 h-7'/>:<IoMenu className='w-7 h-7'/>
       }
     </div>
   </div>  
